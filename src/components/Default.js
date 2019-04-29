5282f4374dcd48563aaede9f5d0aa6cba51c2a33
Austin Oyugi <austinoyugi@gmail.com>
2019-04-29 10:25:32 +0300

    store site app

---
+++ src/components/Default.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import styled from 'styled-components';
+
+class Default extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Page not found</h3>
+            </div>
+        );
+    }
+}
+
+const error404 = styled.div`
+    
+
+`;
+export default Default;
